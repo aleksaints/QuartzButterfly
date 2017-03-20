c77f335ad15cb8d21e9b6b3f005be7b372c4e526
aleksaints <alekspunx@gmail.com>
2017-03-19 22:54:29 -0600

    delete old template files

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('quartzButterfly', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ui.router', 'ui.bootstrap', 'toastr']);
-
-})();
